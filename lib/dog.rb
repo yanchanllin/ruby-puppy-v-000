d12ed6b22a36bddde68da25ca4d50846658329d4
YanC Lin <yonalin1@gmail.com>
2017-11-08 06:15:42 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog 
+  @@all= []
+  attr_accessor :name
+   
+    def initialize(name)
+      @name = name
+      @@all << self
+    end
+
+    def self.all
+    @@all.each do |dog|
+      puts dog.name
+    end
+  end
+end
